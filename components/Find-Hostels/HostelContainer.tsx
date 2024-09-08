f082149159e751ee5caf09c33c36afa41fd4aeec
Joshardals <joshuabamidele219@gmail.com>
2024-09-08 14:37:28 +0100

    initial commit

--- components/Find-Hostels/HostelContainer.tsx
+++ components/Find-Hostels/HostelContainer.tsx
@@ -2,10 +2,14 @@ import { Sorting } from "./Sorting";
 
 export function Hostelcontainer() {
   return (
-    <section className="">
+    <section className="space-y-4">
       <div className="flex items-center justify-between">
         <Sorting />
         <p className="text-charcoal/70"> 1,000 rentals</p>
+      </div>
+
+      <div>
+        <h3>Hostel Listing...</h3>
       </div>
     </section>
   );
